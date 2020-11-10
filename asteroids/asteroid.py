acbe59006c150faade7cb0d7b6ab92b6aad1198b
Steve <stevekentlanier@gmail.com>
2020-11-10 11:43:41 -0800

    installed venv deps

---
+++ asteroids/asteroid.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+while True:
+    # Code
+    # Code
+    #
+    #
+    #
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
